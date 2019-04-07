e4586a57c76b6fe59f696ab5a9624fbd61c8eff8
Anthony M <anthony@followapp.me>
2019-04-07 18:07:23 -0400

    add non-activerecord user class

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User
+  attr_reader :first_name, :last_name, :company, :email, :address1, :address2,
+              :zip, :city, :state_long, :state, :phone
+
+  def initialize(args)
+    @first_name = args[:first_name]
+    @last_name = args[:last_name]
+    @company = args[:company]
+    @email = args[:email]
+    @address1 = args[:address1]
+    @address2 = args[:address2]
+    @zip = args[:zip]
+    @city = args[:city]
+    @state_long = args[:state_long]
+    @state = args[:state]
+    @phone = args[:phone]
+  end
+end
